1655a316b5b4e0bdfe2a006ef0998f7ab5b26eb2
leah_ana <865578847@qq.com>
2022-12-01 10:31:11 +0800

    多线程分而治之

--- 100Days-master/day01-15/day13/countDemo/__init__.py
+++ 100Days-master/day01-15/day13/countDemo/__init__.py
@@ -6,3 +6,19 @@
 @IDE ：PyCharm
 @Motto：ABC(Always Be Coding)
 """
+from time import time
+
+
+def main():
+    total = 0
+    number_list = [x for x in range(1, 100000001)]
+    start = time()
+    for num in number_list:
+        total += num
+    print(total)
+    end = time()
+    print('Execution time : %.3fs' % (end - start))
+
+
+if __name__ == '__main__':
+    main()
