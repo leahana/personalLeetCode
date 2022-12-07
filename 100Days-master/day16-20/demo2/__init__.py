c41ecb52b8d2cc9d8181f5fcb5f0b88d49864970
leah_ana <865578847@qq.com>
2022-12-07 15:22:24 +0800

    归并排序，顺序查找，二分

--- 100Days-master/day16-20/demo2/__init__.py
+++ 100Days-master/day16-20/demo2/__init__.py
@@ -11,30 +11,30 @@ import datetime
 """排序算法"""
 
 
-def select_sort(iteams, comp=lambda x, y: x < y):
+def select_sort(items, comp=lambda x, y: x < y):
     """简单选择排序"""
-    iteams = iteams[:]
-    for i in range(len(iteams) - 1):
+    items = items[:]
+    for i in range(len(items) - 1):
         min_index = i
-        for j in range(i + 1, len(iteams)):
-            if comp(iteams[j], iteams[min_index]):
+        for j in range(i + 1, len(items)):
+            if comp(items[j], items[min_index]):
                 min_index = j
-                iteams[i], iteams[min_index] = iteams[min_index], iteams[i]
-    return iteams
+                items[i], items[min_index] = items[min_index], items[i]
+    return items
 
 
-def bubble_sort(iteams, comp=lambda x, y: x > y):
+def bubble_sort(items, comp=lambda x, y: x > y):
     """冒泡排序"""
-    iteams = iteams[:]
-    for i in range(len(iteams) - 1):
+    items = items[:]
+    for i in range(len(items) - 1):
         swapped = False
-        for j in range(len(iteams) - 1 - i):
-            if comp(iteams[j], iteams[j + 1]):
-                iteams[j], iteams[j + 1] = iteams[j + 1], iteams[j]
+        for j in range(len(items) - 1 - i):
+            if comp(items[j], items[j + 1]):
+                items[j], items[j + 1] = items[j + 1], items[j]
                 swapped = True
         if not swapped:
             break
-    return iteams
+    return items
 
 
 def bubble_sort_plus(items, comp=lambda x, y: x > y):
@@ -57,12 +57,67 @@ def bubble_sort_plus(items, comp=lambda x, y: x > y):
     return items
 
 
+def merge(items1, items2, comp=lambda x, y: x < y):
+    """合并（将两个有序的列表合成一个有序的列表）"""
+    items = []
+    index1, index2 = 0, 0
+    while index1 < len(items1) and index2 < len(items2):
+        if comp(items1[index1], items2[index2]):
+            items.append(items1[index1])
+            index1 += 1
+
+        else:
+            items.append(items2[index2])
+            index2 += 1
+
+    items += items1[index1:]
+    items += items2[index2:]
+    return items
+
+
+def merge_sort(items, comp=lambda x, y: x < y):
+    return _merge_sort(list(items), comp)
+
+
+def _merge_sort(items, comp):
+    """归并排序"""
+    if len(items) < 2:
+        return items
+    mid = len(items) // 2
+    left = _merge_sort(items[:mid], comp)
+    right = _merge_sort(items[mid:], comp)
+    return merge(left, right, comp)
+
+
+def seq_search(items, key):
+    """顺序查找"""
+    for index, item in enumerate(items):
+        if index == key:
+            return index
+    return -1
+
+
+def bin_search(items, key):
+    """折半查找"""
+    start, end = 0, len(items) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if key > items[mid]:
+            start = mid + 1
+        elif key < items[mid]:
+            end = mid - 1
+        else:
+            return mid
+    return -1
+
+
 def main():
     list1 = [5, 3, 21, 55, 6, 2, 11]
     start_time = datetime.datetime.now()
     for _ in range(100000):
-        item = bubble_sort_plus(list1)
+        item = merge_sort(list1)
     end_time = datetime.datetime.now()
+
     print(list1)
     print(item)
     print(end_time - start_time)
