eabe7373b95f205caf0cd5180641a5565c169d4a
leah_ana <865578847@qq.com>
2022-12-16 10:27:39 +0800

    面向对象

---
+++ 100Days-master/day16-20/面向对象相关/__init__.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2022/12/13 10:03
+@Auth ： leah_ana
+@File ：__init__.py.py
+@IDE ：PyCharm
+@Motto：ABC(Always Be Coding)
+"""
+"""
+月薪结算系统 - 部门经理每月15000 程序员每小时200 销售员1800底薪加销售额5%提成
+"""
+
+from abc import ABCMeta, abstractmethod
+
+
+class Employee(metaclass=ABCMeta):
+    """员工（抽象类）"""
+
+    def __init__(self, name):
+        self.name = name
+
+    @abstractmethod
+    def get_salary(self):
+        """结算月薪（抽象方法）"""
+        pass
+
+
+class Manager(Employee):
+    """部门经理"""
+
+    def get_salary(self):
+        return 15000.0
+
+
+class Programmer(Employee):
+    """程序员 """
+
+    def get_salary(self):
+        return 200.0 * self.working_hour
+
+    def __init__(self, name, working_hour=0):
+        self.working_hour = working_hour
+        super().__init__(name)
+
+
+class Salesman(Employee):
+    """销售员"""
+
+    def __init__(self, name, sales=0.0):
+        self.sales = sales
+        super().__init__(name)
+
+    def get_salary(self):
+        return 1800.0 + self.sales * 0.05
+
+
+class EmployeeFactory:
+    """创建员工的工厂（工厂模式 - 通过工厂实现对象使用者和对象之间的解耦合）"""
+
+    @staticmethod
+    def create(emp_type, *args, **kwargs):
+        """创建员工"""
+        all_emp_types = {'M': Manager, 'P': Programmer, 'S': Salesman}
+        cls = all_emp_types[emp_type.upper()]
+        return cls(*args, **kwargs) if cls else None
